8a0ee946604b661d1e9c11e634266f79f12932b5
Ahsan Ihsan <ahsan.ihsan@outlook.com>
2020-10-20 02:20:28 +0500

    Added environment variable functionality

--- project/server.js
+++ project/server.js
@@ -1,23 +1,32 @@
-require('./models/db');
+require("./models/db");
 
-const express = require('express');
-const path = require('path');
-const exphbs = require('express-handlebars');
-const bodyparser = require('body-parser');
+const express = require("express");
+const path = require("path");
+const exphbs = require("express-handlebars");
+const bodyparser = require("body-parser");
 
-const employeeController = require('./controllers/employeeController');
+const employeeController = require("./controllers/employeeController");
 
 var app = express();
-app.use(bodyparser.urlencoded({
-    extended: true
-}));
+app.use(
+  bodyparser.urlencoded({
+    extended: true,
+  })
+);
 app.use(bodyparser.json());
-app.set('views', path.join(__dirname, '/views/'));
-app.engine('hbs', exphbs({ extname: 'hbs', defaultLayout: 'mainLayout', layoutsDir: __dirname + '/views/layouts/' }));
-app.set('view engine', 'hbs');
+app.set("views", path.join(__dirname, "/views/"));
+app.engine(
+  "hbs",
+  exphbs({
+    extname: "hbs",
+    defaultLayout: "mainLayout",
+    layoutsDir: __dirname + "/views/layouts/",
+  })
+);
+app.set("view engine", "hbs");
 
-app.listen(3000, () => {
-    console.log('Express server started at port : 3000');
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Express server started at port : 3000");
 });
 
-app.use('/employee', employeeController);
+app.use("/employee", employeeController);
